b454e52b9038e104c3fcfc3dee40ae5ee6a4ac02
Johnny <navigator_ftp@yahoo.com>
2015-10-19 21:36:54 +0300

    Main program

---
+++ Snake/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Snake
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.SetBufferSize(80, 25);
+			// drow frame
+			HorizontalLine upLine = new HorizontalLine(0,78,0,'*');
+			HorizontalLine downLinw = new HorizontalLine(0,78,24,'*');
+			VerticalLine leftLine = new VerticalLine(0,24,0,'*');
+			VerticalLine rightLine = new VerticalLine(0,24, 78, '*');
+
+			upLine.Draw();
+			downLinw.Draw();
+			leftLine.Draw();
+			rightLine.Draw();
+
+			// drow point
+
+			Point point = new Point(40, 12, 'X');
+			point.Draw();
+
+
+			Console.ReadLine ();
+		}
+	}
+}
